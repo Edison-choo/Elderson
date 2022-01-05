c3c062c09a110c2f1b627e9b9826b03b239a7224
Xuan Wei <joshualim2122@gmail.com>
2022-01-05 23:42:50 +0800

    update

---
+++ Models/FAQ.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elderson.Models
+{
+    public class FAQ
+    {
+        public string Id { get; set; }
+
+        public string Question { get; set; }
+
+        public string Topic { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
--- Pages/AdminHomePage.cshtml.cs
+++ Pages/AdminHomePage.cshtml.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Elderson.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Elderson
 {
     public class AdminHomePageModel : PageModel
     {
+
+        private UserService _svc;
+        private readonly ILogger<AdminHomePageModel> _logger;
+
+        public AdminHomePageModel(ILogger<AdminHomePageModel> logger, UserService service)
+        {
+            _logger = logger;
+            _svc = service;
+        }
         public void OnGet()
         {
 
