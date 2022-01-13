a55b8144de8de778d58a3aee9d364e81149dd4ed
Xuan Wei <joshualim2122@gmail.com>
2022-01-13 11:52:16 +0800

    my db is now gone

---
+++ Pages/PatientList.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elderson.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Elderson
+{
+    public class PatientListModel : PageModel
+    {
+
+        private UserService _svc;
+        private readonly ILogger<PatientListModel> _logger;
+
+        public PatientListModel(ILogger<PatientListModel> logger, UserService service)
+        {
+            _logger = logger;
+            _svc = service;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
