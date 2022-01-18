6655f44efb066aebdd2ad84ca3ee01a559ad1344
yuzhenglim <203139Q@mymail.nyp.edu.sg>
2022-01-18 16:38:31 +0800

    Base Pages Loadup

---
+++ Pages/Pharmacist/Inventory/CreateInventory.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Elderson.Pages.Pharmacist.Inventory
+{
+    public class CreateInventoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Pharmacist/MedSup/SupplierList.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Elderson.Pages.Pharmacist.MedSup
+{
+    public class SupplierListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
