c75f801d94184f88f4b48fc93fc96c19a4c0db85
yuzhenglim <203139Q@mymail.nyp.edu.sg>
2022-01-20 00:56:03 +0800

    Finished view part. Inventory CRUB finished

--- Pages/Pharmacist/Inventory/ViewInventoryObj.cshtml.cs
+++ Pages/Pharmacist/Inventory/ViewInventoryObj.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Elderson.Models;
+using Elderson.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,20 @@ namespace Elderson.Pages.Pharmacist.Inventory
 {
     public class ViewInventoryObjModel : PageModel
     {
-        public void OnGet()
+
+        [BindProperty]
+        public MedInventory SelectedInventoryItem { get; set; }
+        [BindProperty]
+        public Medication SelectedMedication { get; set; }
+        private InventoryService _svc;
+        public ViewInventoryObjModel(InventoryService service)
         {
+            _svc = service;
+        }
+        public void OnGet(string id)
+        {
+            SelectedInventoryItem = _svc.GetInvMedicationById(id);
+            SelectedMedication = _svc.GetMedicationById(id);
         }
     }
 }
