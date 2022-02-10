65f13e0cbdd4acc84afb9a31181583fe8ad98fb6
CruzChua <cruzchua2001@gmail.com>
2022-02-10 09:25:06 +0800

    New DB table

---
+++ Models/Form.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elderson.Models
+{
+    public class Form
+    {
+        public string Id { get; set; }
+        public string DoctorId { get; set; }
+        public string BookingId { get; set; }
+    }
+}
---
+++ Models/FormMeds.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elderson.Models
+{
+    public class FormMeds
+    {
+        public string Id { get; set; }
+        public string FormId { get; set; }
+        public string MedicationId { get; set; }
+    }
+}
