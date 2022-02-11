21b9dd302d987c5266361ad6962506d6783de45c
CruzChua <cruzchua2001@gmail.com>
2022-02-11 09:37:02 +0800

    Cruz

--- Api/FormMedsController.cs
+++ Api/FormMedsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Elderson.Api
@@ -24,8 +25,28 @@ namespace Elderson.Api
             _invsvc = service2;
         }
 
+        // GET: api/<FormMedsController>/form_id
+        [HttpGet("{form_id}")]
+        public ActionResult<List<FormMeds>> Get(string form_id)
+        {
+            List<FormMeds> allform = new List<FormMeds>();
+
+            try
+            {
+                allform = _svc.GetFormMedsByFormId(form_id);
+                var jsonStr = JsonSerializer.Serialize(allform.Select(x => new { x.Id, x.FormId, x.MedicationId, x.Quantity, x.MedName, x.MedType }));
+                return Ok(jsonStr);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("instalmentController.getCarLoan", ex);
+                return BadRequest();
+            }
+
+        }
+
         [HttpPost("{med_id}/{form_id}/{quantity}")]
-        public ActionResult<List<Form>> Post(string med_id, string form_id, int quantity)
+        public ActionResult<List<FormMeds>> Post(string med_id, string form_id, int quantity)
         {
             FormMeds formMed = new FormMeds();
             formMed.Id = Guid.NewGuid().ToString();
@@ -42,6 +63,22 @@ namespace Elderson.Api
             catch (Exception ex)
             {
                 Console.WriteLine("UserController.DeleteUser", ex);
+            }
+            return Ok();
+        }
+
+        // DELETE api/<FormMedsController>/id
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+                var deleteFormmed = _svc.GetFormMedsById(id);
+                _svc.DeleteFormMed(deleteFormmed);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FormMedsController.DeleteFormMed", ex);
             }
             return Ok();
         }
--- Services/FormMedsService.cs
+++ Services/FormMedsService.cs
@@ -1,4 +1,5 @@
 ﻿using Elderson.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +15,55 @@ namespace Elderson.Services
             _context = context;
         }
 
+        private bool FormMedExist(string id)
+        {
+            return _context.Schedule.Any(e => e.Id == id);
+        }
+
+        public List<FormMeds> GetFormMedsByFormId(string form_id)
+        {
+            List<FormMeds> allformmeds = new List<FormMeds>();
+            allformmeds = _context.FormMeds.Where(e => e.FormId == form_id).ToList();
+            return allformmeds;
+        }
+
+        public FormMeds GetFormMedsById(string id)
+        {
+            FormMeds formmed = new FormMeds();
+            formmed = _context.FormMeds.Where(e => e.Id == id).FirstOrDefault();
+            return formmed;
+        }
+
         public bool AddFormMeds(FormMeds formMeds)
         {
             _context.Add(formMeds);
             _context.SaveChanges();
             return true;
+        }
+
+        public bool DeleteFormMed(FormMeds formMeds)
+        {
+            bool deleted = true;
+            _context.Attach(formMeds).State = EntityState.Modified;
+
+            try
+            {
+                _context.Remove(formMeds);
+                _context.SaveChanges();
+                deleted = true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FormMedExist(formMeds.Id))
+                {
+                    deleted = false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return deleted;
         }
     }
 }
